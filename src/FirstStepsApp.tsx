3bf666041d6fb0dedb59c08ab11fe11f5f718593
asantott <asantos@tecnologiatransaccional.com>
2026-03-19 15:12:25 -0600

    Init Commit

---
+++ src/FirstStepsApp.tsx
@@ -0,0 +1,12 @@
+export function FirstStepsApp() {
+    return (
+        <>
+            <h1>Hola Mundo!!</h1>
+            <p>Esto es un parrafo</p>
+
+            <div>
+                <h2>Hola dentro de un div</h2>
+            </div>
+        </>
+    );
+}
